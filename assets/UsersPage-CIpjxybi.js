79fc799a93cf8c70685115bb6fbc2d66ea012542
MateusPitura <mateuspitura@gmail.com>
2025-01-16 14:06:55 -0300

    Updates

--- assets/UsersPage-CIpjxybi.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ChRquggA.js";function s(){return e.jsx("div",{children:"Users Page"})}export{s as default};
--- assets/VehiclesPage-B2Vvtbp_.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ChRquggA.js";function r(){return e.jsx("div",{children:"Vehicle Page"})}export{r as default};
